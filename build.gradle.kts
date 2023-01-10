cfe8c03ab680e2d4a9e4c24bc9e0c91c8926c463
Федотов Михаил Викторович <MFedotov2@alfabank.ru>
2023-01-10 12:39:51 +0300

    Test java8 compatibility with jitpack and mavenCenatral artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.0"// 1.7.0 ok 1.4.0 ok, 1.6.0 ok
+    kotlin("jvm") version "1.6.0"// 1.7.0 ok 1.5.0 fail
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     //implementation(fileTree("libs") { include("*.jar") })
     testImplementation(kotlin("test"))
-    implementation("io.github.nsk90:kstatemachine:0.15.0")
+    //implementation("io.github.nsk90:kstatemachine:0.17.0")
+    implementation("com.github.nsk90:kstatemachine:jitpack2-SNAPSHOT")
 }
 
 tasks.test {
@@ -24,3 +27,11 @@ tasks.test {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+// JavaVersion.VERSION_11 ok with kotlin 1.6 1.7
+// JavaVersion.VERSION_1_8 ok with kotlin 1.6 1.7 - fixed
